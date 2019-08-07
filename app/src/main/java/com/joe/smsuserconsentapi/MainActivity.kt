1f10cfe6ca2d7bff99b106122a482b1bb9b8e5c2
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-08-07 17:30:51 +0300

    Start listening for incoming messages

--- app/src/main/java/com/joe/smsuserconsentapi/MainActivity.kt
+++ app/src/main/java/com/joe/smsuserconsentapi/MainActivity.kt
@@ -2,11 +2,30 @@ package com.joe.smsuserconsentapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.phone.SmsRetriever
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun startSmsUserConsent() {
+        SmsRetriever.getClient(this).also {
+            it.startSmsUserConsent("+254780433770")
+                .addOnSuccessListener {
+                    Log.d(TAG, "success")
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "failure")
+                }
+        }
+    }
+
+    companion object {
+        const val TAG = "SMS User Consent API"
+        const val REQ_USER_CONSENT = 100
     }
 }
